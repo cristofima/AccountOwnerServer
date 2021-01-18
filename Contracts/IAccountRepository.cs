c27956fe704fc4126e0c9cc9164fd1c30209d6c5
Cristopher Coronado Moreira <cristofima@hotmail.com>
2021-01-18 17:35:03 -0500

    Init project

---
+++ Contracts/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Contracts
+{
+    public interface IAccountRepository : IRepositoryBase<Account>
+    {
+        IEnumerable<Account> AccountsByOwner(Guid ownerId);
+        IEnumerable<Account> GetAllAccounts();
+        void CreateAccount(Account account);
+    }
+}
---
+++ Entities/DataTransferObjects/AccountForCreationDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DataTransferObjects
+{
+    public class AccountForCreationDto
+    {
+        public DateTime DateCreated { get; set; }
+        public string AccountType { get; set; }
+    }
+}
